33475d320d7aa77b876925e96cab106fe24507ef
werniq <qniwwwersss@gmail.com>
2023-01-11 22:06:49 +0100

    Working on payment & login

--- cmd/api/api-handlers.go
+++ cmd/api/api-handlers.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
 	"unicode"
@@ -15,20 +16,84 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-// type stripePayload struct {
-// 	Currency      string `json:"currency"`
-// 	Amount        string `json:"amount"`
-// 	PaymentMethod string `json:"payment_method"`
-// 	Email         string `json:"email"`
-// 	CardBrand     string `json:"card_brand"`
-// 	ExpiryMonth   int 	 `json:"exp_month"`
-// 	ExpiryYear    int 	 `json:"exp_year"`
-// 	LastFour      string `json:"last_four"`
-// 	Plan          string `json:"plan"`
-// 	ProductID     string `json:"product_id"`
-// 	FirstName     string `json:"first_name"`
-// 	LastName      string `json:"last_name"`
-// }
+
+type jsonResponse struct {
+	OK      bool   `json:"ok"`
+	Message string `json:"message,omitempty"`
+	Content string `json:"content,omitempty"`
+	ID      int    `json:"id,omitempty"`
+}
+
+
+type stripePayload struct {
+	Currency      string `json:"currency"`
+	Amount        string `json:"amount"`
+	PaymentMethod string `json:"payment_method"`
+	Email         string `json:"email"`
+	CardBrand     string `json:"card_brand"`
+	ExpiryMonth   int 	 `json:"exp_month"`
+	ExpiryYear    int 	 `json:"exp_year"`
+	LastFour      string `json:"last_four"`
+	Plan          string `json:"plan"`
+	ProductID     string `json:"product_id"`
+	FirstName     string `json:"first_name"`
+	LastName      string `json:"last_name"`
+}
+
+func (app *application) GetPaymentIntent(w http.ResponseWriter, r *http.Request) {
+	var payload stripePayload
+
+	err := json.NewDecoder(r.Body).Decode(&payload)
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	amount, err := strconv.Atoi(payload.Amount)
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	card := cards.Card{
+		Secret:   app.cfg.stripe.secret,
+		Key:      app.cfg.stripe.key,
+		Currency: payload.Currency,
+	}
+
+	okay := true
+
+	pi, msg, err := card.Charge(payload.Currency, amount)
+	if err != nil {
+		okay = false
+	}
+
+	if okay {
+		out, err := json.MarshalIndent(pi, "", "   ")
+		if err != nil {
+			app.errorLog.Println(err)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(out)
+	} else {
+		j := jsonResponse{
+			OK:      false,
+			Message: msg,
+			Content: "",
+		}
+
+		out, err := json.MarshalIndent(j, "", "   ")
+		if err != nil {
+			app.errorLog.Println(err)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(out)
+	}
+}
+
 
 type TransactionData struct {
 	FirstName       string
